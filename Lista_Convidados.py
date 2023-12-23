87efd7ec3bd62fb2636244c8ae8e238f11afcea1
Leo <120729803+G0bust3r@users.noreply.github.com>
2023-12-23 18:02:27 -0300

    Add files via upload

---
+++ Lista_Convidados.py
@@ -0,0 +1,35 @@
+'''
+Exercicio: Faça um programa que leia a quantidade de pessoas que serão convidadas para uma festa;
+Após isso o programa ira perguntar o nome de todas as pessoas e colocar numa lista de convidados;
+Após isso irá imprimir todos os nomes da lista.
+'''
+print('#################################')
+print ('#### Controle de convidados: #### ')
+print('#################################\n')
+
+quant_convidados = int (input ('Qual a quantidade de convidados ? '))
+print ('\nQuantidade de convidados: ', quant_convidados)
+print('\n')
+
+lista_de_convidados = []
+
+contador = 1
+
+while contador <= quant_convidados: # Se fosse com for: for contador in range(int(quant_convidados))
+     name_convidado = input ('Qual nome do convidado ' + str (contador) + ': ')
+     lista_de_convidados.append(name_convidado)
+     contador += 1
+
+contador_convidados = 1
+
+print('\nQuantidade Convidados: ', quant_convidados )
+print('\nLista de Convidados: ') 
+for i, convidado in enumerate (lista_de_convidados, start=1):
+ print (str(i), '-', (convidado)) 
+
+# Mensagem final
+print('\nPrograma concluído. Obrigado por utilizar o Controle de Convidados!')
+
+
+
+
